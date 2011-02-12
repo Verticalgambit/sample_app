17b0f90419fa1d3c40efb19dd51e24f0f00f717c
verticalgambit <verticalgambit@gmail.com>
2011-02-12 18:55:26 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '712ce6ae89be4be9b2194a4443ce6434df1e7de26c84ec6aff07e2d354e670d75beb9d0d70127e4d30b93b616e60a6124d9db6a1c64d8227b67308f21add9028'
